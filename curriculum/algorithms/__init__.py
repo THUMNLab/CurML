f675c81a5ca6bc4abc8fcd7c50767884a1327c35
zhouyw16 <zhouyw1216@163.com>
2022-05-03 15:34:19 +0800

    combine

--- curriculum/algorithms/__init__.py
+++ curriculum/algorithms/__init__.py
@@ -1,19 +1,29 @@
-from .base import BaseTrainer
+from .base import BaseCL, BaseTrainer
 
-from .baby_step import BabyStepTrainer
-from .lambda_step import LambdaStepTrainer
-from .self_paced import SelfPacedTrainer
-from .transfer_teacher import TransferTeacherTrainer
-from .superloss import SuperlossTrainer
+from .baby_step import BabyStep, BabyStepTrainer
+from .lambda_step import LambdaStep, LambdaStepTrainer
+from .self_paced import SelfPaced, SelfPacedTrainer
+from .transfer_teacher import TransferTeacher, TransferTeacherTrainer
+from .superloss import Superloss, SuperlossTrainer
 
 
 
 all = [
+    'BaseCL'
     'BaseTrainer',
 
+    'BabyStep',
     'BabyStepTrainer',
+
+    'LambdaStep',
     'LambdaStepTrainer',
+
+    'SelfPaced',
     'SelfPacedTrainer',
+
+    'TransferTeacher',
     'TransferTeacherTrainer',
+
+    'Superloss',
     'SuperlossTrainer',
 ]
---
+++ examples/baby_step+superloss.py
@@ -0,0 +1,46 @@
+import math
+import argparse
+
+from curriculum.solvers import ImageClassifier
+from curriculum.algorithms import BabyStep, Superloss
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data', type=str, default='cifar10')
+parser.add_argument('--net', type=str, default='resnet')
+parser.add_argument('--seed', type=int, default='42')
+parser.add_argument('--device', type=str, default='cuda')
+parser.add_argument('--start_rate', type=float, default=0.0)
+parser.add_argument('--grow_rate', type=float, default=0.1)
+parser.add_argument('--grow_interval', type=int, default=20)
+parser.add_argument('--tau', type=float, default=math.log(10))
+parser.add_argument('--lam', type=float, default=1.0)
+parser.add_argument('--fac', type=float, default=0.0)
+args = parser.parse_args()
+
+
+baby_step = BabyStep(
+    start_rate=args.start_rate,
+    grow_rate=args.grow_rate,
+    grow_interval=args.grow_interval,
+)
+
+superloss = Superloss(
+    tau=args.tau,
+    lam=args.lam,
+    fac=args.fac,
+)
+
+trainer = ImageClassifier(
+    data_name=args.data,
+    net_name=args.net,
+    device_name=args.device,
+    random_seed=args.seed,
+    algorithm_name='babystep+superloss',
+    data_curriculum=baby_step.data_curriculum, 
+    model_curriculum=baby_step.model_curriculum, 
+    loss_curriculum=superloss.loss_curriculum
+)
+trainer.fit()
+trainer.evaluate()
